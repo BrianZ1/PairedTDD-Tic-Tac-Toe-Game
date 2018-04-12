379fb14b15c4df2a78ccf5c06c9a23f9edcb473d
BrianZ1 <brian.zhang@mymail.champlain.edu>
2018-04-11 20:34:50 -0400

    tests for placing on border and check winner

--- Paired TDD/Source.cpp
+++ Paired TDD/Source.cpp
@@ -1,21 +1,19 @@
 #include "Header.h"
 
 void initBoard(char board[][BOARD_SIZE]) {
-	
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < BOARD_SIZE; i++)
 	{
-		for (int j = 0; j < 5; j++) 
+		for (int j = 0; j < BOARD_SIZE; j++)
 		{
 			board[i][j] = '#';
 		}
 	}
-
 }
 
 void displayBoard(char board[][BOARD_SIZE]) {
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < BOARD_SIZE; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < BOARD_SIZE; j++)
 		{
 			cout << board[i][j] << " ";
 		}
